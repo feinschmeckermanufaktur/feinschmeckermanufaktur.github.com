4c1aaeaf03540ee70493a0a9b0f276410fe3870e
dpree <whiterabbit.init@gmail.com>
2011-07-12 17:16:12 +0200

    moved assets and included stylesheets again

--- config.rb
+++ config.rb
@@ -6,10 +6,10 @@ Bundler.require :style
 
 # Set this to the root of your project when deployed:
 http_path = "."
-css_dir = "_stylesheets"
+css_dir = "stylesheets"
 sass_dir = "style"
 images_dir = "../images"
-javascripts_dir = "_javascripts"
+javascripts_dir = "javascripts"
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
